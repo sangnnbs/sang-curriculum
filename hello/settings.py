11ecddc01810408f87ba09d2345c4714607c8923
nguyennhubaosang@gmail.com <nguyennhubaosang@gmail.com>
2021-10-31 11:55:43 +0700

    Mime

--- hello/settings.py
+++ hello/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import mimetypes
 
+# Add mimetypes for CSS
+mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
