75b3025202cac875f96e86175311dd4c627ded93
nguyennhubaosang@gmail.com <nguyennhubaosang@gmail.com>
2021-10-31 13:46:41 +0700

    them url static

--- hello/urls.py
+++ hello/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls import url
+
+from django.conf import settings
+
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('resume.urls'))
+    path('', include('resume.urls')),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
