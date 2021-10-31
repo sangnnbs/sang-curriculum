b08e342fb6cc00104da4dbb7f06327a6ca3aaead
nguyennhubaosang@gmail.com <nguyennhubaosang@gmail.com>
2021-10-31 13:24:40 +0700

    change staticfiles

--- hello/settings.py
+++ hello/settings.py
@@ -153,7 +153,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 # Simplified static file serving.
