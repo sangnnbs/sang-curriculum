792dca7c1781d530664d67299fb98a6d86df5b4c
nguyennhubaosang@gmail.com <nguyennhubaosang@gmail.com>
2021-11-10 21:26:46 +0700

    ok

--- resume/views.py
+++ resume/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 def homepage(request):
 	return render(request = request, template_name="resume/home.html")
 
 def profile(request):
-	return render(request = request, template_name="resume/index.html")
+	profile = Profile.objects.get(pk = 1)
+
+	context = {
+        'profile': profile,
+    }
+
+	return render(request = request, template_name="resume/index.html", context=context)
