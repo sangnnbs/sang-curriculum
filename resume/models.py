b249d56859feb68514ae23661613c7a9e37a1abe
nguyennhubaosang@gmail.com <nguyennhubaosang@gmail.com>
2021-11-18 16:41:49 +0700

    db project

--- resume/models.py
+++ resume/models.py
@@ -3,11 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Profile(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, default='Sang')
     phone_number = models.CharField(max_length=30)
-    email = models.CharField(max_length=50)
+    email = models.EmailField()
     github = models.CharField(max_length=100)
     bio = models.CharField(max_length=500)
     avatar = models.CharField(max_length=100)
     facebook = models.CharField(max_length=100)
+    project_link1 = models.CharField(max_length=100, blank=True)
+    project_link2 = models.CharField(max_length=100, blank=True)
+    project1 = models.TextField(null=True)
+    project2 = models.TextField(null=True)
     
